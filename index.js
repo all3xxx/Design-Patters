b072b86f45237a604e7101f59edb84c43a402f7d
Allex <1allex.moura@gmail.com>
2024-02-07 19:01:54 -0300

    Implementação Design Patters

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const {DiceRoll, BasicDiceRoll, DiceRollDecorator} = require("./Decorator");
+
+app.use(express.json());
+
+
+// Rota para lidar com o lançamento de dados
+
+app.get('/roll', (req, res) => {
+    const basicRoll = new BasicDiceRoll();
+    const resultado_dados = basicRoll.roll();
+    res.json({ resultado_dados });
+});
+
+
+// Inicializa o servidor
+app.listen(4000, () => {
+    console.log(`Servidor rodando em http://localhost:${4000}`);
+});
