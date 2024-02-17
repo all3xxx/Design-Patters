522a8a6c76d63a4532b64715d6398a53a6420210
Allex <112523623+all3xxx@users.noreply.github.com>
2024-02-17 18:14:20 -0300

    Update Decorator.js

--- Decorator.js
+++ Decorator.js
@@ -1,20 +1,20 @@
 //Eu como usuário, quero lançar os dados para defender os meus territórios//
 
-// Interface para os lançamentos de dados
+
 class DiceRoll {
     roll() {
         throw new Error('Método abstrato e deve ser implementado pelas subclasses.');
     }
 }
 
-// Implementação concreta da classe de lançamento de dados
+// Implementação da classe para o lançamento dos dados
 class BasicDiceRoll extends DiceRoll {
     roll() {
-        return Math.floor(Math.random() * 6) + 1; // Simula o lançamento de um dado de 6 faces
+        return Math.floor(Math.random() * 6) + 1; // Simula o lançamento do dado
     }
 }
 
-// Decorator para lançamentos de dados
+// Decorator para lançamentos dos dados
 class DiceRollDecorator extends DiceRoll {
     constructor(diceRoll) {
         super();
@@ -27,7 +27,7 @@ class DiceRollDecorator extends DiceRoll {
 }
 
 
-// Exemplo de uso
+
 const basicRoll = new BasicDiceRoll();
 console.log("Lançamento básico:", basicRoll.roll());
 
